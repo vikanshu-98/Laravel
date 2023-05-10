49680ffb56335a7b28f7b7ca7a9dc2c741bb4b57
vikanshu-98 <vikanshu8954985003@gmail.com>
2023-05-10 17:29:23 +0530

    small change

--- Practice/routes/api.php
+++ Practice/routes/api.php
@@ -40,3 +40,4 @@ Route::get('get-group-with-successor-mutator',function(){
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+//
